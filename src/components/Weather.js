cd9a0945afe881354360b89e071643a6f56f0efb
dkimd5 <dkimd5@mail.ru>
2022-02-26 17:10:19 +0300

    Add degree sign

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -25,7 +25,7 @@ function Weather() {
 
   return (
     <WeatherContainer>
-      <Temp>{data.main.temp.toFixed(0)}</Temp>
+      <Temp>{data.main.temp.toFixed(0)}&#176;</Temp>
       <City>Moscow</City>
     </WeatherContainer>
   );
