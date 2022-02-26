4733d4ddf734f1e6a519a24ec02f2a593e1b3a7f
dkimd5 <dkimd5@mail.ru>
2022-02-26 16:40:27 +0300

    Add api url for quotes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const url = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
 
 function Quote() {
   return <div>Quote</div>;
