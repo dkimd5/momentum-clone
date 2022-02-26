8b51d8cd24ab82c50fbaac56c367dc7297bb10d7
dkimd5 <dkimd5@mail.ru>
2022-02-26 16:33:13 +0300

    Add date display

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Date() {
-  return <div>Date</div>;
+  let today = new Date();
+  console.log(today);
+  let date =
+    today.getFullYear() + "/" + today.getMonth() + "/" + today.getDate();
+
+  return (
+    <div>
+      <p>{date}</p>
+    </div>
+  );
 }
 
 export default Date;
