f709b4d9b3436d41fe92a0ac17a7eb6eed6b40ad
dkimd5 <dkimd5@mail.ru>
2022-02-26 16:27:02 +0300

    Import date comp to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Time from "./components/Time";
 import Weather from "./components/Weather";
+import Date from "./components/Date";
 
 function App() {
   return (
     <div>
       <Weather />
       <Time />
+      <Date />
     </div>
   );
 }
