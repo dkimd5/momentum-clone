51323388ebf35404782fd85dfed0555da6123434
dkimd5 <dkimd5@mail.ru>
2022-02-26 16:06:05 +0300

    Add axios request for weather data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=moscow&appid=126d418eddb672618ccf7add92fbf7ac";
 
 function Weather() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(data);
+
+  if (!data) return null;
+
   return <div>Weather</div>;
 }
 
